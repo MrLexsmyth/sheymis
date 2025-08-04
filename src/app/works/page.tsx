cbc52794cb80fc1141ca5117b60401dcb75a64c6
MrLexsmyth <shittuodunayo4@gmail.com>
2025-08-04 19:26:06 +0100

    update

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import Image from "next/image";
+
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="mt-16 flex justify-center items-center px-4">
+      <div className=" mt-16 w-full max-w-3xl flex flex-col items-center">
+        <Image
+          src="/images/artist.jpeg"
+          alt="Logo"
+          width={800}
+          height={700}
+          className="w-full h-auto rounded-lg"
+        />
 
-export default page
+        <h2 className="font-abel font-semibold mt-4 text-center text-lg md:text-xl">
+          - Feeds -
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
